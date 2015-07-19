c8b6c09f795cc5e1e5b63bf5367b07106395b00f
EC2 Default User <ec2-user@ip-172-31-35-14.ec2.internal>
2015-07-19 09:05:54 +0000

    uninstall mysql

--- scripts/change_permissions.sh
+++ scripts/change_permissions.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-chmod -R 644 /var/www/html/WordPress
+chown -R apache:apache /var/www/html/WordPress
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -3,7 +3,3 @@ isExistApp=`pgrep httpd`
 if [[ -n  \$isExistApp ]]; then
    service httpd stop
 fi
-isExistApp=`pgrep mysqld`
-if [[ -n  \$isExistApp ]]; then
-    service mysqld stop
-fi
